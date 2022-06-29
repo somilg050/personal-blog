b6422796afeb4be9fd8e9d195915560ab56ebc00
somilg050 <somil@telematica.so>
2022-06-30 01:45:19 +0530

    Minor fix in alignment

--- theme.config.js
+++ theme.config.js
@@ -4,7 +4,7 @@ export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
       <time>{YEAR}</time> © Somil Gupta.
-      Made with <span className="heart">&hearts;</span> in India.
+      <a>Made with <span className="heart">&hearts;</span> in India.</a>
       <style jsx>{`
         a {
           float: right;
